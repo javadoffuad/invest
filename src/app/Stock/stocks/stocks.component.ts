1b05c5fe3be219225dac2edd3cc0c3397efea014
fuad.dzhavadov1 <fuad.dzhavadov@idp.zyfra.com>
2024-11-16 12:26:07 +0300

    chore(app): taiga material instead of material

--- src/app/Stock/stocks/stocks.component.ts
+++ src/app/Stock/stocks/stocks.component.ts
@@ -1,23 +1,24 @@
 import { Component, signal } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import {AsyncPipe, CurrencyPipe, NgForOf} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {TuiTable} from '@taiga-ui/addon-table';
+import {TuiFormatNumberPipe} from '@taiga-ui/core';
 import { IActivitySector, IStock } from '../models/stock.models';
-import { StockItemComponent } from "../stock-item/stock-item.component";
 import {StocksService} from '../services/stocks.service';
 import {ActivitySectorsService} from '../services/activity-sectors.service';
-import {CurrencyPipe} from '@angular/common';
-import {RouterLink} from '@angular/router';
+import {TuiCurrencyPipe} from '@taiga-ui/addon-commerce';
 
 @Component({
   selector: 'app-stocks',
   standalone: true,
-  imports: [StockItemComponent, MatTableModule, CurrencyPipe, RouterLink],
+  imports: [CurrencyPipe, RouterLink, TuiTable, TuiFormatNumberPipe, NgForOf, AsyncPipe, TuiCurrencyPipe],
   templateUrl: './stocks.component.html',
   styleUrl: './stocks.component.less'
 })
 export class StocksComponent {
   protected activitySectors = signal<IActivitySector[]>([]);
   protected stocks = signal<IStock[]>([]);
-  protected readonly displayedColumns: string[] = ['id', 'name', 'price', 'minPriceIncrement'];
+  protected readonly displayedColumns: string[] = ['name', 'price', 'absolutePrice'];
 
   constructor(
     private stocksService: StocksService,
