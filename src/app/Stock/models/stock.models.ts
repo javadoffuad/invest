c53d39e38d343a56f837a1d7efbdaf96a9017abb
Фуад Джавадов <fuad.dzhavadov@idp.zyfra.com>
2024-11-16 10:20:54 +0300

    chore(stocks) extends stock model

--- src/app/Stock/models/stock.models.ts
+++ src/app/Stock/models/stock.models.ts
@@ -1,10 +1,27 @@
 
+// "earnings": {
+//   "absolute": {
+//     "currency": "RUB",
+//       "value": -0.65
+//   },
+//   "relative": -0.0084,
+//     "previousPrice": {
+//     "currency": "RUB",
+//       "value": 77.25
+//   }
+// },
+
 export interface IStock {
   id: number;
   name: string;
   shortName: string;
   price: IPrice;
   lotSize: number;
+  earnings: {
+    absolute: IPrice;
+    previousPrice: IPrice;
+    relative: number;
+  },
   sectorId: IActivitySector['id'];
 }
 
--- src/app/Stock/stocks/stocks.component.ts
+++ src/app/Stock/stocks/stocks.component.ts
@@ -5,18 +5,19 @@ import { StockItemComponent } from "../stock-item/stock-item.component";
 import {StocksService} from '../services/stocks.service';
 import {ActivitySectorsService} from '../services/activity-sectors.service';
 import {CurrencyPipe} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-stocks',
   standalone: true,
-  imports: [StockItemComponent, MatTableModule, CurrencyPipe],
+  imports: [StockItemComponent, MatTableModule, CurrencyPipe, RouterLink],
   templateUrl: './stocks.component.html',
   styleUrl: './stocks.component.less'
 })
 export class StocksComponent {
   protected activitySectors = signal<IActivitySector[]>([]);
   protected stocks = signal<IStock[]>([]);
-  protected readonly displayedColumns: string[] = ['id', 'name', 'price'];
+  protected readonly displayedColumns: string[] = ['id', 'name', 'price', 'minPriceIncrement'];
 
   constructor(
     private stocksService: StocksService,
