d964868761c0a27d5d443d1cbc1f9dbc3f52b52c
Фуад Джавадов <javadoffuad@gmail.com>
2024-11-29 00:24:34 +0300

    chore(Stocks) stock review

--- src/app/Stock/pages/stock-detail/components/stock-review/stock-review.component.ts
+++ src/app/Stock/pages/stock-detail/components/stock-review/stock-review.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-stock-review',
   standalone: true,
   imports: [],
   templateUrl: './stock-review.component.html',
-  styleUrl: './stock-review.component.less'
+  styleUrl: './stock-review.component.less',
 })
 export class StockReviewComponent {
-
+  brandName = signal('ГК Самолет');
 }
