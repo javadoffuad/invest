5dbad318d9d7f0bad8d99f2f2989945d38d732a0
Фуад Джавадов <javadoffuad@gmail.com>
2024-12-01 00:39:30 +0300

    chore(Stocks) test

--- src/app/Stock/pages/stock-detail/components/stock-review/stock-review.component.ts
+++ src/app/Stock/pages/stock-detail/components/stock-review/stock-review.component.ts
@@ -1,22 +1,21 @@
-import {Component, inject, signal, Signal} from '@angular/core';
-import {IStock} from '../../../../models/stock.models';
-import {ROUTER_OUTLET_DATA} from '@angular/router';
-import {CompaniesService} from '../../../../services/companies/companies.service';
-import {ICompany} from '../../../../models/company.models';
+import { Component, inject, signal, Signal } from '@angular/core';
+import { IStock } from '../../../../models/stock.models';
+import { ROUTER_OUTLET_DATA } from '@angular/router';
+import { CompaniesService } from '../../../../services/companies/companies.service';
+import { ICompany } from '../../../../models/company.models';
 
 @Component({
-    selector: 'app-stock-review',
-    imports: [],
-    templateUrl: './stock-review.component.html',
-    styleUrl: './stock-review.component.less'
+  selector: 'app-stock-review',
+  imports: [],
+  templateUrl: './stock-review.component.html',
+  styleUrl: './stock-review.component.less',
 })
 export class StockReviewComponent {
   protected stock$ = inject<Signal<IStock>>(ROUTER_OUTLET_DATA);
   protected company$ = signal<ICompany | null>(null);
+  protected country = '';
 
-  constructor(
-    private companies: CompaniesService
-  ) {
+  constructor(private companies: CompaniesService) {
     const stock = this.stock$();
 
     if (stock) {
