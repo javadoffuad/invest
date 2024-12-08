f51c23fe067259b0fe4c9f712ce1ef0d2c799644
javadoffuad <javadoffuad@gmail.com>
2024-12-08 14:29:32 +0300

    chore(Invest): invest search styling

--- src/app/Invest/components/invest-home/invest-home.component.ts
+++ src/app/Invest/components/invest-home/invest-home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CatalogSearchComponent } from '../catalog-search/catalog-search.component';
-import { TuiInputModule } from '@taiga-ui/legacy';
+import { TuiInputModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'app-invest-home',
-  imports: [RouterOutlet, CatalogSearchComponent, TuiInputModule, ReactiveFormsModule],
+  imports: [
+    RouterOutlet,
+    CatalogSearchComponent,
+    TuiInputModule,
+    ReactiveFormsModule,
+    TuiTextfieldControllerModule,
+  ],
   templateUrl: './invest-home.component.html',
   styleUrl: './invest-home.component.less',
 })
