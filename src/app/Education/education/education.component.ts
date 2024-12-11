c7a3468f822a15c4bfb1c73f49b8ec68491641d9
Фуад Джавадов <javadoffuad@gmail.com>
2024-12-11 23:41:09 +0300

    chore(Stocks) add education component

---
+++ src/app/Education/education/education.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  imports: [],
+  templateUrl: './education.component.html',
+  styleUrl: './education.component.less',
+})
+export class EducationComponent {}
