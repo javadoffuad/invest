d5752f58fe3857868976cc3ce2eb92bd7b3fa977
Фуад Джавадов <javadoffuad@gmail.com>
2024-12-12 00:33:26 +0300

    chore(Invest) update currencies service

--- src/app/Invest/Stock/pages/stocks/services/feature-currencies/feature-currencies.service.ts
+++ src/app/Invest/Stock/pages/stocks/services/feature-currencies/feature-currencies.service.ts
@@ -1,18 +1,18 @@
 import { Injectable, Signal } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { StocksActions } from '../../../../store/stocks/actions/stocks.actions';
+import { CurrenciesActions } from '../../../../store/currencies/actions/currencies.actions';
 import { IStock } from '../../../../../models/stock.models';
-import * as Selectors from '../../../../store/stocks/selectors/stocks.selectors';
+import * as Selectors from '../../../../store/currencies/selectors/currencies.selectors';
 
 @Injectable()
 export class FeatureCurrenciesService {
   constructor(private store: Store) {}
 
-  public getStocks(): void {
-    this.store.dispatch(StocksActions.loadStocks());
+  public getCurrencies(): void {
+    this.store.dispatch(CurrenciesActions.loadCurrencies());
   }
 
-  public selectStocks(): Signal<IStock[]> {
-    return this.store.selectSignal(Selectors.selectStocks);
+  public selectCurrencies(): Signal<IStock[]> {
+    return this.store.selectSignal(Selectors.selectCurrencies);
   }
 }
