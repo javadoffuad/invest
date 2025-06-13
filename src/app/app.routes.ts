1588798132cf701efd007e701290101f1c4b767d
Фуад Джавадов <javadoffuad@gmail.com>
2025-06-13 14:52:29 +0300

    refactor(app) remove useless pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,31 +3,15 @@ import { investRoutes } from './Invest/invest.routes';
 
 /**
  * /
- * invest routes
- * education
- * account
+ * invest
  * login
  */
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./Home/home/home.component').then((c) => c.HomeComponent),
-  },
   ...investRoutes,
   {
     path: 'login',
     loadComponent: () => import('./Login/login/login.component').then((c) => c.LoginComponent),
-  },
-  {
-    path: 'education',
-    loadComponent: () =>
-      import('./Education/education/education.component').then((c) => c.EducationComponent),
-  },
-  {
-    path: 'account',
-    loadComponent: () =>
-      import('./Account/account/account.component').then((c) => c.AccountComponent),
   },
   {
     path: '**',
